ec2fc885794311d4d53b50c810c3fd5867d43a3f
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-09-12 12:55:57 +0000

    Learnt new values

--- Main.java
+++ Main.java
@@ -3,6 +3,21 @@ public class Main {
 // main method is the ENTRYWAY to the program
    public static void main(String []args) {
       System.out.println("Welcome to Java!");
-      system.out.println("hiiii");
+      System.out.println("hiiii");
+
+      // 1. DECLARE a new variable
+      int score;
+      // 2. ASSIGN a value to the variable
+      score = 72;
+      // INITIALIZE combines declaring + assigning
+      String pokemon = "Pikachu";
+      double healthPercent = 0.75;
+      boolean isAlive = true;
+
+      // Use the plus sign to CONCATENATE (combine) Strings
+      String greeting = "Hello " + "World";
+      System.out.println(greeting);
+      // Quotation marks contain STRING LITERALS
+
    }
 }
